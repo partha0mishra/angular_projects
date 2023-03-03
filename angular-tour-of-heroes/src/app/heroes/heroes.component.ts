7c48854931f5ab5651538329d4ae526c527533bd
Partha Mishra <partha.mishra@gmail.com>
2023-03-02 21:06:56 -0600

    4b - show msgs at svc and usage

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,25 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit{
+  selectedHero?: Hero;
   heroes: Hero[] = [];
-  constructor(private heroService: HeroService){};
+  constructor(private heroService: HeroService,
+    private messageService: MessageService){};
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+    this.messageService.add(`Heroes Component: Selected hero ID= ${hero.id}`);
+  }
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    this.getHeroes();
-  }
+  
+  
+  
 }
