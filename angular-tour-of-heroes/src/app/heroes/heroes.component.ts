cc48d7689401843e00347ae6cffb14a39ffbbb44
Partha Mishra <partha.mishra@gmail.com>
2023-03-02 20:23:55 -0600

    Step 4 - create service

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService){};
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
   selectedHero?: Hero;
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getHeroes();
   }
 }
